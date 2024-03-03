b17ead82be8962e5da7f2359f9ad9ca035310364
devrlobs <dev.rommellobiano@gmail.com>
2024-03-03 23:58:32 +0800

    code clean up and additional setup settings

--- SecureCash_ATM_Web_App/SecureCash_ATM_Web_App/obj/Debug/net6.0/SecureCash_ATM_Web_App.AssemblyInfo.cs
+++ SecureCash_ATM_Web_App/SecureCash_ATM_Web_App/obj/Debug/net6.0/SecureCash_ATM_Web_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecureCash_ATM_Web_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f82a42d565b4dd5e047ae9f8e9e11cdf7c3a871")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6e34adf971c126e336c6b730dc5ae3b0bcd9189")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecureCash_ATM_Web_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecureCash_ATM_Web_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
